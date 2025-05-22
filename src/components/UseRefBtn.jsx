995ccaec1b523f48e8cdbfc61e09b5234b4b662f
RubiyaHud <rubiya.hud@gmail.com>
2025-05-22 15:07:58 +0600

    change name

---
+++ src/components/UseRefBtn.jsx
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+
+const UseRefBtn = () => {
+
+    const clickCount = useRef(0);
+    function handleClick() {
+        clickCount.current += 1;
+        console.log(clickCount)
+        console.log(`Button clicked ${clickCount.current} times`);
+        // The component does NOT re-render when clickCount changes
+    }
+
+    return (
+        <>
+            <div className='max-w-[1170px] m-auto text-center'>
+                <h1 className='bg-amber-100'>useRef button</h1>
+                <p>You clicked {clickCount.current} times</p>
+                <button onClick={handleClick} className='cursor-pointer p-5 mt-2.5 rounded-2xl bg-cyan-200' >
+                    Click me
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default UseRefBtn
